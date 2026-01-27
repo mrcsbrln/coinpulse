d12eaa53322faf97d0ba7a07f6dd9c0132818a29
mrcsbln <hrtmnn.mrcs@gmail.com>
2026-01-27 14:28:24 +0100

    progress

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+import CoinOverview from "@/components/home/CoinOverview";
+import TrendingCoins from "@/components/home/TrendingCoins";
+import { Suspense } from "react";
 
-const page = () => {
-  return <div></div>;
+const Page = async () => {
+  return (
+    <main className="main-container">
+      <section className="home-grid">
+        <Suspense fallback={<div>Loading Overview...</div>}>
+          <CoinOverview />
+        </Suspense>
+        <Suspense fallback={<div>Loading Trending...</div>}>
+          <TrendingCoins />
+        </Suspense>
+      </section>
+      <section className="w-full mt-7 space-y-4">
+        <p>Categories</p>
+      </section>
+    </main>
+  );
 };
 
-export default page;
+export default Page;
